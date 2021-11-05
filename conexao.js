9b8254f46e8b3316a36ce7b16e1e461b4fe2d854
juliocesar1316 <juliocesar1316@gmail.com>
2021-11-05 12:19:21 -0300

    Deploy da aplicação

--- conexao.js
+++ conexao.js
@@ -1,11 +1,15 @@
-const knex = require('knex')({
-  client: 'pg',
+const knex = require("knex")({
+  client: "pg",
   connection: {
-    host : 'localhost',
-    port : 5432,
-    user : 'postgres',
-    password : '123456789',
-    database : 'sharenergy'
-  }
+    host: "ec2-54-211-174-60.compute-1.amazonaws.com",
+    port: 5432,
+    user: "ehambmeefhawzr",
+    password:
+      "ebc171fd34ce9bf27458015e446b9d8ccbf98d5904009c3150dad14ad01d0737",
+    database: "d7l9l8b34qkd9e",
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 });
-module.exports = knex
+module.exports = knex;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const rotas = require('./rotas.js');
-const cors = require('cors');
+const express = require("express");
+const rotas = require("./rotas.js");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(rotas);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
