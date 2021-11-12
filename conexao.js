391f899d4a45d4eac40369abb0553a656e69dd1b
juliocesar1316 <juliocesar1316@gmail.com>
2021-11-12 13:33:57 -0300

    Instalação do env

--- conexao.js
+++ conexao.js
@@ -1,12 +1,11 @@
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: "ec2-54-211-174-60.compute-1.amazonaws.com",
+    host: process.env.DB_HOST,
     port: 5432,
-    user: "ehambmeefhawzr",
-    password:
-      "ebc171fd34ce9bf27458015e446b9d8ccbf98d5904009c3150dad14ad01d0737",
-    database: "d7l9l8b34qkd9e",
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     ssl: {
       rejectUnauthorized: false,
     },
